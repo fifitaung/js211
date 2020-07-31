137c5b9255ff47f84e17bcbdc0bce09887f658f8
User <user@MacBook-Pro.local>
2020-07-30 19:35:42 -0500

    changes

---
+++ hw1.js
@@ -0,0 +1,12 @@
+'use strict'
+
+
+// function to input something 
+
+
+
+// This is declaring and assigning a variable for the current date & time
+const currentDate = () => {
+    console.log("The current date & time " + new Date())
+}
+currentDate();
