ed96d3f51be4f5e933a8fc3cb2d12753895c47ff
User <user@MacBook-Pro.local>
2020-08-06 17:56:11 -0500

    expected results

--- weekOne/hw2.js
+++ weekOne/hw2.js
@@ -12,12 +12,37 @@ const rl = readline.createInterface({
 });
 
 // the function that will be called by the unit test below
-const rockPaperScissors = (hand1, hand2) => {
+let rock = "rock"
+let paper = "paper"
+let scissors = "scissors"
 
+const rockPaperScissors = (hand1, hand2) => {
   // Write code here
   // Use the unit test to see what is expected
+hand1 = hand1.toLowerCase();
+hand2 = hand2.toLowerCase();
+hand1 = hand1.trim();
+hand2 = hand2.trim();
+
+if(hand1 === hand2){
+  return "It's a tie!"
+}
+else if (hand1 === rock && hand2 === scissors ||hand1 === paper && hand2 === rock ||hand1 === scissors && hand2 === paper) {
+  return "Hand one wins!"
+  }
+else if(hand2 === rock && hand1 === scissors ||hand2 === paper && hand1 === rock ||hand2 === scissors && hand1 === paper){
+  return "Hand two wins!"
+  }
+  else{
+    return "Incorrect input try again!!!"
+
+  }
+  
+
 
 }
+
+
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
